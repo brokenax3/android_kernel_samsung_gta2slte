bf130a903dbbf8e1ba65bde787e9c518f5372d69
Mark <mlcy0508@gmail.com>
2021-02-09 11:54:10 +1100

    gta2slte: Device specific changes

--- build_kernel.sh
+++ build_kernel.sh
@@ -7,13 +7,15 @@ OUT_DIR=out
 # ex)CROSS_COMPILE={android platform directory you downloaded}/android/prebuilt/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 COMMON_ARGS="-C $(pwd) O=$(pwd)/${OUT_DIR} ARCH=arm CROSS_COMPILE=arm-linux-androideabi- KCFLAGS=-mno-android"
 
-export PATH=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin:$PATH
+export PATH=$(pwd)/../prebuilts/arm-linux-androideabi-4.9/bin:$PATH
 export ARCH=arm
+export SUBARCH=arm
+export THREADS=$(nproc --all)
 
 [ -d ${OUT_DIR} ] && rm -rf ${OUT_DIR}
 mkdir ${OUT_DIR}
 
-make ${COMMON_ARGS} gta2slte_kor_skt_defconfig
-make -j64 ${COMMON_ARGS}
+make ${COMMON_ARGS} gta2slte_defconfig
+make -j${THREADS} ${COMMON_ARGS}
 
 cp ${OUT_DIR}/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
